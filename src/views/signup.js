bfecca5751d9c7272ce7c266645a8313d1ef2968
Sintra <sintradacosta@icloud.com>
2025-07-09 22:55:15 +0100

    se hacen los estilos del sign up

--- src/views/signup.js
+++ src/views/signup.js
@@ -10,26 +10,29 @@ export function Signup(container) {
   newUserdiv.classList.add("divNewUser");
   container.appendChild(newUserdiv);
   newUserdiv.innerHTML = `
-    <form id = "divNewUserForm">
-    <h2 id="registerFormH2">Register form</h2>
-    <input type="text" id="registerFormName" placeholder="Name" required>
-    <input type="email" id="registerFormEmail" placeholder="Email" required>
-    <input type="password" id="registerFormPassword" placeholder="Password" required>
-    <input type="password" id="registerFormRepPassword" placeholder="Repeat password" required>
-    <label for="registerFormIsland">Island of residence</label>
-    <select name="registerFormIsland" id="registerFormIsland">
-     <option value="" disabled selected>Select your island</option>
-     <option value="Tfe">Tenerife</option>
-     <option value="LPa">La Palma</option>
-     <option value="Gom">La Gomera</option>
-     <option value="Hrr">El Hierro</option>
-     <option value="GranC">Gran Canaria</option>
-     <option value="Fvra">Fuerteventura</option>
-     <option value="Lzte">Lanzarote</option>
-     <option value="LGra">La Graciosa</option>
-    </select>
-    <button type="submit">Sign up</button>
-    </form>`;
+    <div class="div-form-sign-up">
+       <form id = "divNewUserForm">
+         <h2 id="registerFormH2" class="sign-up-title">Register form</h2>
+         <input type="text" class= "signup-input" id="registerFormName" placeholder="(*)Name" required>
+         <input type="email" id="registerFormEmail"class="signup-input" placeholder="(*)Email" required>
+         <input type="password" id="registerFormPassword" class= "signup-input" placeholder="(*)Password" required>
+         <input type="password" id="registerFormRepPassword" class= "signup-input"placeholder="(*)Repeat password" required>
+         <label for="registerFormIsland" class="sign-up-text-label">(*)Island of residence</label>
+         <select name="registerFormIsland" id="registerFormIsland"  class= "signup-input">
+            <option value="" disabled selected>Select your island</option>
+            <option value="Tfe" >Tenerife</option>
+            <option value="LPa">La Palma</option>
+            <option value="Gom">La Gomera</option>
+            <option value="Hrr">El Hierro</option>
+            <option value="GranC">Gran Canaria</option>
+            <option value="Fvra">Fuerteventura</option>
+            <option value="Lzte">Lanzarote</option>
+            <option value="LGra">La Graciosa</option>
+         </select>
+         <small class="small-sign-up">(*)All fields are required.</small>
+         <button type="submit" class= "button-large button-outline">SIGN UP</button>
+       </form>
+     </div>`;
 
   const formNewUser = document.getElementById("divNewUserForm");
 
